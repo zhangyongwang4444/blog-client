c9b3b3872b658400e42d59ba8e008a0ca73dab4f
zhangyongwang <349368992@qq.com>
2018-09-18 16:50:14 +0800

    项目结构，组件样式1

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+      <p>xiedaimala.com</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1>Let's share</h1>
+    <p>精品博客汇聚</p>
+    <div class="btns">
+      <el-button>立即登录</el-button>
+      <el-button>注册账号</el-button>
+    </div>
+  </header>
+</template>
+
+<style lang="less">
+@import "../assets/base.less";
+
+header {
+  padding: 0 12% 30px 12px;
+  background: @bgColor;
+  display: grid;
+  justify-items: center;
+
+  h1 {
+    color: #fff;
+    font-size: 40px;
+    margin: 60px 0 0 0;
+    text-transform: uppercase;
+  }
+
+  p {
+    margin: 15px 0 0 0;
+    color: #fff;
+  }
+
+  .btns{
+    margin-top: 20px;
+  }
+
+  button {
+    margin: 20px 5px 0;
+  }
+}
+</style>
+
