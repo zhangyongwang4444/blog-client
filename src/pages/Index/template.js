ad20e1e22c643883ac887024bd46794e7b137e9e
zhangyongwang <349368992@qq.com>
2018-09-18 09:22:58 +0800

    api接口封装-blog

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,8 +1,12 @@
 import request from '@/helpers/request.js'
 import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
+
+
 
 window.request = request
 window.auth = auth
+window.blog = blog
 
 export default {
   data() {
