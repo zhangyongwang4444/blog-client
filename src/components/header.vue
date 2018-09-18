993875b7cd0657f2c7f8ba463aa78fedf4bc5fb4
zhangyongwang <349368992@qq.com>
2018-09-18 17:37:03 +0800

    header.login header.no-login 不同样式

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,18 +1,36 @@
 <template>
-  <header>
-    <h1>Let's share</h1>
-    <p>精品博客汇聚</p>
-    <div class="btns">
-      <el-button>立即登录</el-button>
-      <el-button>注册账号</el-button>
-    </div>
+  <header :class="{login:isLogin,'no-login':!isLogin}">
+    <template v-if="!isLogin">
+      <h1>Let's share</h1>
+      <p>精品博客汇聚</p>
+      <div class="btns">
+        <el-button>立即登录</el-button>
+        <el-button>注册账号</el-button>
+      </div>
+    </template>
+    <template v-if="isLogin">
+      <h1>Let's share</h1>
+      <i class="edit el-icon-edit"></i>
+      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" class="avatar">
+    </template>
+    
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLogin: true
+    };
+  }
+};
+</script>
 
 <style lang="less">
 @import "../assets/base.less";
 
-header {
+header.no-login {
   padding: 0 12% 30px 12px;
   background: @bgColor;
   display: grid;
@@ -30,12 +48,39 @@ header {
     color: #fff;
   }
 
-  .btns{
+  .btns {
     margin-top: 20px;
   }
 
   button {
     margin: 20px 5px 0;
+  }
+}
+
+header.login {
+  display: flex;
+  align-items: center;
+  background: @bgColor;
+  h1 {
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    font-size: 40px;
+    text-transform: uppercase;
+    flex: 1;
+  }
+
+  .edit {
+    color: #fff;
+    font-size: 30px;
+  }
+
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    margin-left: 15px;
   }
 }
 </style>
