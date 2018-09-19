c690acd4a8bcf245e376b335ada66dbde777fc02
zhangyongwang <349368992@qq.com>
2018-09-19 16:14:32 +0800

    优化header中的 立即登录/注册账号<router-link>

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,8 +4,8 @@
       <h1>Let's share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
-        <el-button>立即登录</el-button>
-        <el-button>注册账号</el-button>
+        <router-link to="/login"><el-button>立即登录</el-button></router-link>
+        <router-link to="/register"><el-button>注册账号</el-button></router-link>
       </div>
     </template>
     <template v-if="isLogin">
