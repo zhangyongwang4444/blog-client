d571e27f745b3e470e81e3467e6fad80dd02acfd
zhangyongwang <349368992@qq.com>
2018-09-19 16:03:38 +0800

    完善header-- 我的/注销

--- src/components/header.vue
+++ src/components/header.vue
@@ -11,14 +11,20 @@
     <template v-if="isLogin">
       <h1>Let's share</h1>
       <i class="edit el-icon-edit"></i>
-      <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username" >
+      <div class="user">
+        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
+        <ul>
+          <li><router-link to="my">我的</router-link></li>
+          <li><a href="#" @click="onLogout">注销</a></li>
+        </ul>
+      </div>         
+      
     </template>
     
   </header>
 </template>
 
 <script>
-
 import auth from "@/api/auth";
 window.auth = auth;
 
@@ -35,7 +41,10 @@ export default {
     this.checkLogin();
   },
   methods: {
-    ...mapActions(["checkLogin"])
+    ...mapActions(["checkLogin", "logout"]),
+    onLogout() {
+      this.logout();
+    }
   }
 };
 </script>
@@ -94,6 +103,36 @@ header.login {
     border: 1px solid #fff;
     border-radius: 50%;
     margin-left: 15px;
+  }
+  .user {
+    position: relative;
+
+    ul {
+      display: none;
+      position: absolute;
+      right: 0;
+      list-style: none;
+      border: 1px solid #eaeaea;
+      margin: 0;
+      padding: 0;
+      background-color: #fff;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-size: 12px;
+        display: block;
+        padding: 5px 10px;
+
+        &:hover {
+          background-color: #eaeaea;
+        }
+      }
+    }
+
+    &:hover ul {
+      display: block;
+    }
   }
 }
 </style>
